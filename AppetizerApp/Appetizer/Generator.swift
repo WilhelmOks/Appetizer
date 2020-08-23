403f4ed18085b0717a97e741766d77de5595cd3c
Wilhelm Oks <>
2020-08-23 14:41:21 +0200

    -

--- AppetizerApp/Appetizer/Generator.swift
+++ AppetizerApp/Appetizer/Generator.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import AppKit
 
 final class Generator {
     public static let shared = Generator()
@@ -14,6 +15,18 @@ final class Generator {
     private init() {}
     
     func execute(_ tasks: [Task]) {
-        
+        do {
+            for task in tasks {
+                let inputImage = try NSImage.from(filePath: task.inputPath)
+                let filteredOutputTasks = task.outputTasks.filter{ !$0.deleted && $0.enabled && $0.isReady }
+                for outputTask in filteredOutputTasks {
+                    let scaledImage = inputImage.scaled(toSize: CGSize(width: 16, height: 16), padding: 0)
+                    let outputUrl = URL(fileURLWithPath: outputTask.outputPath)
+                    try scaledImage.saveAsPng(fileUrl: outputUrl)
+                }
+            }
+        } catch let error {
+            print(error)
+        }
     }
 }
