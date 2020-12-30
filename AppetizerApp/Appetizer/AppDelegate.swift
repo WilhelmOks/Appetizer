d39cf7d71d7a5ddea8b8801ba9d03700200c8b42
Wilhelm Oks <wilhelm.oks@gmx.de>
2020-12-30 17:18:11 +0100

    terminate after last window closed

--- AppetizerApp/Appetizer/AppDelegate.swift
+++ AppetizerApp/Appetizer/AppDelegate.swift
@@ -11,9 +11,7 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     var window: NSWindow!
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
@@ -35,6 +33,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
 
